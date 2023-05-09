55b7351509a8de3944d03d5c7283d0c75cdf6e9a
ANDRÉ LUIZ CONSTANTINO <22282216@dsn127m1246730.cfp127aluno.local>
2023-05-09 17:01:24 -0300

    Finalizado

---
+++ app/src/main/java/br/senai/sp/jandira/rickandmorty/service/CharactersService.kt
@@ -0,0 +1,26 @@
+package br.senai.sp.jandira.rickandmorty.service
+
+import br.senai.sp.jandira.rickandmorty.model.CharacterList
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CharactersService {
+
+    @GET("character")
+    fun getCharacter(): Call<CharacterList>
+
+    @GET("character/{id}")
+    fun getCharacterById(@Path("id") id: Long): Call<Character>
+
+    @GET("character/")
+    fun getCharactersByPage(@Query("page") page: Int): Call<CharacterList>
+
+    //https://rickandmortyapi.com/api/character/?name=rick&status=alive
+    @GET("character/")
+    fun getCharacterByNameAndStatus(
+        @Query("name") name: String,
+        @Query("status") status: String
+    ): Call<CharacterList>
+}
